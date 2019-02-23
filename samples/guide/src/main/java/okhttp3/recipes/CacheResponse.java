e0c0ddde5d53f1af6a4bcfcbe65e8db2727ec2c3
Ragnar Rova <ragnar.rova@svt.se>
2019-02-23 13:33:03 +0100

    Allow different types of response caches to be used
    Refactor so that you can provide your own custom response cache
    instead of the the default filesystem-based cache.

--- samples/guide/src/main/java/okhttp3/recipes/CacheResponse.java
+++ samples/guide/src/main/java/okhttp3/recipes/CacheResponse.java
@@ -17,10 +17,8 @@ package okhttp3.recipes;
 
 import java.io.File;
 import java.io.IOException;
-import okhttp3.Cache;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+
+import okhttp3.*;
 
 public final class CacheResponse {
   private final OkHttpClient client;
